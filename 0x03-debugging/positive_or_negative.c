486156e2b891e1b91d77e35b2cbd5e909cf8fb3f
RadwaMohamedMoustafa <radwa.feshir@gmail.com>
2023-03-23 19:39:46 -0700

    0x03. C - Debugging

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,26 +1,21 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
 /**
-* main - Prints a random number and states whether
-*        it is positive, negative, or zero.
-*
-* Return: Always 0.
-*/
-int main(void)
+ * main - Entry point
+ *
+ * Description: Program that will determine if an integer is -tive or +tive
+ *
+ * @i: integer
+ *
+ * Return: always 0
+ */
+
+void positive_or_negative(int i)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n < 0)
-		printf("%d is negative\n", n);
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i < 0)
+		printf("%d is negative\n", i);
 	else
-		printf("%d is zero\n", n);
-
-	return (0);
+		printf("%d is zero\n", i);
 }
