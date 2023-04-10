6f7c47dbc36d5fc9aa792a46464989d1ff340fcc
RadwaMohamedMoustafa <radwa.feshir@gmail.com>
2023-04-09 20:36:52 -0700

    0x0A. C - argc, argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,4 +1,5 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - Prints the addition of positive numbers,
@@ -9,24 +10,25 @@
  * Return: If one of the numbers contains symbols that are non-digits - 1.
  *         Otherwise - 0.
  */
-
 int main(int argc, char *argv[])
 {
 	int num, digit, sum = 0;
 
 	for (num = 1; num < argc; num++)
 	{
-		for (digit = 0; argv[num][digit] != '\0'; digit++)
+		for (digit = 0; argv[num][digit]; digit++)
 		{
 			if (argv[num][digit] < '0' || argv[num][digit] > '9')
 			{
-				printf("Error \n");
+				printf("Error\n");
 				return (1);
 			}
 		}
+
 		sum += atoi(argv[num]);
 	}
-printf("%d\n", sum);
 
-return (0);
+	printf("%d\n", sum);
+
+	return (0);
 }
