b96326405ce47e42d0322dad55b4b9ec96e9a880
RadwaMohamedMoustafa <radwa.feshir@gmail.com>
2023-04-09 17:49:59 -0700

    0x0A. C - argc, argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 #include "main.h"
 
-int main (int argc, char *argv[])
+/**
+ * main - Entry point
+ *
+ * Description: prints the number of arguments
+ * @argc: the number of arguments passed
+ * @argv: array of arguments
+ * Return: the number of arguments
+ */
+
+int main(int argc, char *argv[])
 {
 	(void)argv;
-	printf("%d\n", argc -1);
+	printf("%d\n", argc - 1);
 	return (0);
 }
