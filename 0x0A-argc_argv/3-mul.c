ffb0b922b0840ab65d813527277ae7bd517800b6
RadwaMohamedMoustafa <radwa.feshir@gmail.com>
2023-04-09 19:07:54 -0700

    0x0A. C - argc, argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main -  Entry point
+ *
+ * Description: prints multiplication
+ * of two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: returns 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	else if (argc == 3)
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	}
+
+	return (0);
+}
