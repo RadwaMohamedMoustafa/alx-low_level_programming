c4d178200e474f325ce5df7107cb05b396ae4763
RadwaMohamedMoustafa <radwa.feshir@gmail.com>
2023-04-27 04:27:34 -0700

    Counting the elements inside the singly linked list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -12,13 +12,16 @@ size_t list_len(const list_t *h)
 {
 	size_t n = 0;
 
+	if (h == NULL)
+	{
+	return (0);
+	}
+
 	while (h != NULL)
 	{
 	n++;
 	h = h->next;
-
 	}
-	putchar('\n');
 
 	return (n);
 }
