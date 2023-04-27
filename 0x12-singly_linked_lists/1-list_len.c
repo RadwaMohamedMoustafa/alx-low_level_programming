8f3f978d6e1c0d2be9a4e1cfaa584cafb646992f
RadwaMohamedMoustafa <radwa.feshir@gmail.com>
2023-04-27 04:19:48 -0700

    Counting the elements inside the singly linked list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * list_len - Entry point
+ *
+ * Description: Number of elements in a list
+ * @h: Pointer to the head of list
+ * Return: Number of element
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t n = 0;
+
+	if (h == NULL)
+	{
+	return (0);
+	}
+
+	while (h != NULL)
+	{
+	n++;
+	h = h->next;
+
+	}
+	putchar('\n');
+
+	return (n);
+}
