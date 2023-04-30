beb4bd4507e30c8e4c654e0ed404d9431d2fdfd1
RadwaMohamedMoustafa <radwa.feshir@gmail.com>
2023-04-30 05:47:59 -0700

    Counting the nodes inside the singly linked list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * listlin_len - Returns the number of elements
+ * @h: The head of the list
+ *
+ * Return: the number of nodes
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+	h = h->next;
+	count++;
+	}
+	return (count);
+}
