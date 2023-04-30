48db30a4ed1b6202f8c02958ac26b97a54d4e3aa
RadwaMohamedMoustafa <radwa.feshir@gmail.com>
2023-04-30 09:50:29 -0700

    Free a singly linked list with prototype of double pointer

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Frees a list
+ * @head: The beggining of the list
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr = NULL, *temp;
+
+	temp = *head;
+	while (temp)
+	{
+	ptr = temp;
+	temp = temp->next;
+	free(ptr);
+	}
+	*head = NULL;
+}
