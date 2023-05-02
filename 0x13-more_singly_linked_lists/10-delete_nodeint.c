58c694fba1f6b1a00c756aa3962ea2f0616a4947
RadwaMohamedMoustafa <radwa.feshir@gmail.com>
2023-05-02 07:12:06 -0700

    Deleting a specific node in the singly linked list

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,20 +13,22 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *previous;
 	unsigned int i = 0;
 
+	current = *head;
+	previous = *head;
+
 	if (!*head)
 	{
 	return (-1);
 	}
-
-	current = *head;
-	previous = *head;
 
 	if (index == 0 || index == 1)
 	{
 	*head = current->next;
 	free(current);
 	current = NULL;
+	return (1);
 	}
+
 	else
 	{
 	while (index != 1 && i < index)
