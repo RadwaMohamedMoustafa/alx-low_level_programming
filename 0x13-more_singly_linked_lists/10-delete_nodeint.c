3454ab0079fa92f45d2c7f6a49658eaa944336a9
RadwaMohamedMoustafa <radwa.feshir@gmail.com>
2023-05-02 06:46:12 -0700

    Deleting a specific node in the singly linked list

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - Deletes a node at the specified index
+ * @head: The start of the list
+ * @index: The index to delete
+ * Return: 1 if succesful and -1 otherwise
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *current, *previous;
+	unsigned int i = 0;
+
+	current = *head;
+	previous = *head;
+
+	if (!*head)
+	{
+	return (-1);
+	}
+
+
+	if (index == 0 || index == 1)
+	{
+	*head = current->next;
+	free(current);
+	current = NULL;
+	}
+	else
+	{
+	while (index != 1 && i < index)
+	{
+	previous = current;
+	current = current->next;
+	i++;
+	}
+	previous->next = current->next;
+	free(current);
+	current = NULL;
+	}
+	return (1);
+}
