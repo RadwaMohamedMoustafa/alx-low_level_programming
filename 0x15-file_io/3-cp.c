bcc5ef379bd9ce37b9f1dd4678527c74f874c29b
RadwaMohamedMoustafa <radwa.feshir@gmail.com>
2023-05-10 03:08:09 -0700

    A program that copies the content of a file to another file.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -61,7 +61,7 @@ int main(int ac, char **av)
 	dprintf(STDERR_FILENO, "%s %s\n", "Error: Can't read from file", av[1]);
 	exit(98);
 	}
-	fd2 = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	fd2 = open(av[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
 
 	if (fd2 == -1)
 	{
